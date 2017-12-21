c885c523613ab1e2b5575496751ec39847f26af0
Hack Reactor Student <student@hackreactor.com>
2017-12-20 17:37:33 -0800

    woooooooooooooow

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,50 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var http = require('./http-helpers.js');
+var fs = require('fs');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+
+  // if (fs.exists('/Users/student/hrsf86-web-historian/archives/sites' + req.url)) {
+
+
+  // }
+
+
+  if (req.method === 'GET') {
+   
+    if (req.url === '/') {
+      //render html
+      http.serveAssets(res, '/Users/student/hrsf86-web-historian/web/public/index.html', function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        //console.log(data);
+        res.write(data);
+        res.end(JSON.stringify(data));
+      });
+    } else if (req.url !== '/') {
+      // if req.url is already in sites folder
+       // return the contents
+      // console.log('/Users/student/hrsf86-web-historian/archives/sites' + req.url);
+      console.log(path.join(__dirname, req.url));
+      http.serveAssets(res, '/Users/student/hrsf86-web-historian/test/testdata/sites' + req.url, function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        console.log(data);
+        res.write(data);
+        res.end(JSON.stringify(data));
+      });
+      
+    }
+
+  }
+  
+  // res.end(archive.paths.list);
 };
+
+
+
+ // http.serveAssets(res, './public/index.html', function(err, data) {
+ //      res.writeHead(200, {'Content-Type': 'text/html'});
+ //      res.write(data);
+ //      res.end(JSON.stringify(data));
+ //    });
